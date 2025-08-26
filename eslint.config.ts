5ea9013df7e2b2ce29f1f4bf5ccf6ce68e60f315
pukeeee <pukee88@proton.me>
2025-08-27 00:44:51 +0300

    fix: ESLint fix

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,10 +3,21 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
+import vitest from "eslint-plugin-vitest";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {
+    files: ["**/*.test.ts"],
+    plugins: { vitest },
+    rules: vitest.configs.recommended.rules,
+    languageOptions: {
+        globals: {
+            ...vitest.environments.env.globals,
+        }
+    }
+  }
 ]);
